854830e698a1f13dd247ab443a586245a0ab323c
elizabeth-bruce <elizabeth.q.bruce@gmail.com>
2020-01-05 16:12:46 -0500

    Allow command-line args to define the width and height of the generated map

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,15 @@
 module Main where
 
+import System.Environment (getArgs)
+import System.Exit (exitWith)
 import System.Random (newStdGen)
+
 import MapGen.BoundUseCases (boundCreateConsoleViewMap)
 
 main :: IO ()
 main = do
   g <- newStdGen
-  putStr $ boundCreateConsoleViewMap g 250 65
+  [widthRaw, heightRaw] <- getArgs
+  let width = read widthRaw :: Int
+  let height = read heightRaw :: Int
+  putStr $ boundCreateConsoleViewMap g width height
